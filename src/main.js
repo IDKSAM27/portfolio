5d630dcccf5d8711f6d82b74c125865583e63e5b
Sampreet Patil <sampreetpatil270@gmail.com>
2024-10-31 13:26:21 +0530

    added documentation file

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import { k } from "./kaboomCtx";
 
-k.loadSprite("spritesheet", "./spritesheet.png")
+//The width of the spritesheet.png is 624 pixels and each block is 16 px, so simply divide and you'll get 39
+//Same formula for slixeY, 496 px and each block 16 px, we get 31
+k.loadSprite("spritesheet", "./spritesheet.png", {
+    sliceX: 39, 
+    sliceY: 31,
+});
