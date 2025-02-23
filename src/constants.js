856bef1e8180b356d540299ff3e64f5d716d4c13
Sampreet Patil <sampreetpatil270@gmail.com>
2025-02-23 13:51:06 +0530

    Exit button, some content and graphics to the buttons added.

--- src/constants.js
+++ src/constants.js
@@ -4,11 +4,11 @@ export const dialogueData = {
     pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
     I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
     Here is my <a href="https://github.com/idksam27" target="_blank">Github</a>!`,
-    degree: ``,
+    degree: `I'm currently percieving Bachelors in Computer Science and Business Systems from Visvesvaraya Technological University, Belagavi, India`,
     "sofa-table": ``,
     resume: ``,
-    projects: ``,
+    projects: `Visit my <a href="https://github.com/idksam27" target="_blank">Github profile</a>`,
     library: ``,
-    bed: ``,
-    exit: ``,
+    bed: `This is where I think, and sometimes sleep.`,
+    exit: `Exit`,
 } 
