1a9adf13703e98323f40648536f93e9993361e9d
Sampreet Patil <sampreetpatil270@gmail.com>
2025-03-18 23:32:04 +0530

    constants updated in constants.js, upto satisfaction..

--- src/constants.js
+++ src/constants.js
@@ -5,10 +5,10 @@ export const dialogueData = {
     I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
     Here is my <a href="https://github.com/idksam27" target="_blank">Github</a>!`,
     degree: `I'm currently percieving Bachelors in Computer Science and Business Systems from Visvesvaraya Technological University, Belagavi, India`,
-    "sofa-table": ``,
-    resume: ``,
+    "sofa-table": `I code, I don't watch TV, insta, shorts, some time youtube videos`,
+    resume: `Still working on it..`,
     projects: `Visit my <a href="https://github.com/idksam27" target="_blank">Github profile</a>`,
-    library: ``,
+    library: `Still working on it..`,
     bed: `This is where I think, and sometimes sleep.`,
     exit: `Click Exit button to visit my portfolio.`,
 } 
