a333039b7217499802815caa4a56be4798542a5c
Sampreet Patil <sampreetpatil270@gmail.com>
2025-10-18 00:06:26 +0530

    Update dialogueData to reflect new interests and links

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,21 @@
 export const scaleFactor = 4; 
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    pc: `This is my PC. I work mostly in Python these days.
+    I've made a couple of projects and application in that language. I also like Dart and Rust. Anyway regardless of the language, I just like programming.
     Here is my <a href="https://github.com/idksam27" target="_blank">Github</a>!`,
+    
     degree: `I'm currently percieving Bachelors in Computer Science and Business Systems from Visvesvaraya Technological University, Belagavi, India`,
+    
     "sofa-table": `I code, I don't watch TV, insta, shorts, some time youtube videos`,
-    resume: `Still working on it..`,
+    
+    resume: `Hey wanna check out my resume? <a href="https://sampreetpatil.com/resume" target="_blank">Here you go</a>!`,
+    
     projects: `Visit my <a href="https://github.com/idksam27" target="_blank">Github profile</a>`,
-    library: `Still working on it..`,
+    
+    library: `I write about tech and stuff I learnt while building projects, Check out my <a href="https://sampreetpatil.com/blog" target="_blank">blog</a>!`,
+    
     bed: `This is where I think, and sometimes sleep.`,
+    
     exit: `Click Exit button to visit my portfolio.`,
 } 
